7ba2fc65699adca6a87817f423ef061f26fdf790
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-13 18:14:59 +0000

    added route for receiving messages

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,9 +18,8 @@ module.exports = function (app, passport, io) {
 		});
 		
 	io.on('connection', function(socket){
-		console.log('a user connected');
-		socket.on('disconnect', function(){
-    		console.log('user disconnected');
+		socket.on('chat message', function(msg){
+    		console.log('message: ' + msg);
 		});
 	});
 
